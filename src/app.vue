9414e71880e255642d2438b6b1faf36cef820114
renj <renj8477@fxiaoke.com>
2020-07-28 11:51:30 +0800

    renjie

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+
+<template>
+<div>
+    <fx-radio v-model="radio" label="1">备选项</fx-radio>
+    <fx-radio v-model="radio" label="2">备选项</fx-radio>
+    <div><p>lallalal</p></div>
+</template>
+<style> 
+   
+
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        radio:"1",
+      };
+    }
+  };
+</script>
